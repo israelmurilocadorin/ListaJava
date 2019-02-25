9762ddecbb4fe55e01815d2fe7bd3f1fb82139b1
israel <israel.cadorin@catolicasc.org.br>
2019-02-25 15:49:42 -0300

    Retangulo

--- Retangulo/Retangulo.java
+++ Retangulo/Retangulo.java
@@ -3,36 +3,51 @@ public class Retangulo {
     private double largura = 1;
     private double comprimento = 1;
 
-    public boolean setComprimento(double comprimento) {
-        if(comprimento > 0 || comprimento != largura){
-            this.comprimento = comprimento;
-            return true;
-        } else {
+    public Retangulo(double comprimento, double largura) {
+        this.comprimento = comprimento;
+        this.largura = largura;
+    }
+
+    public double ValidarComprimento(double comprimento){
+        if(comprimento < 0 || comprimento == largura){
             System.out.println("Erro: Um dos lados do retângulo é igual ou menor que zero.");
-            return false;
         }
+        return comprimento;
+    }
+
+    public double ValidarLargura(double largura) {
+        if (largura < 0 || largura == comprimento) {
+            System.out.println("Erro: Um dos lados do retângulo é igual ou menor que zero.");
+        }
+        return largura;
+    }
+
+    public void setComprimento(double comprimento) {
+        this.comprimento = ValidarComprimento(comprimento);
     }
 
     public double getComprimento() {
         return comprimento;
     }
 
-    public boolean setLargura(double largura) {
-        if(largura > 0 || largura != comprimento) {
-            this.largura = largura;
-            return true;
-        } else {
-            System.out.println("Erro: Um dos lados do retângulo é igual ou menor que zero.");
-            return false;
-        }
+    public void setLargura(double largura) {
+        this.largura = ValidarLargura(largura);
     }
 
     public double getLargura() {
         return largura;
     }
 
-    public double Calcular(double comprimento, double largura) {
-        double area = comprimento + largura;
-        return area;
+    public double CalcularArea() {
+        return this.largura * this.comprimento;
+    }
+
+    public double CalculadorPerimetro(){
+        return (2 * this.comprimento) + (2 * this.largura);
+    }
+
+    @Override
+    public String toString(){
+        return ("Area: " + CalcularArea() + "\nPeriemtro: " + CalculadorPerimetro());
     }
 }
--- Retangulo/RetanguloTeste.java
+++ Retangulo/RetanguloTeste.java
@@ -1,13 +1,12 @@
 public class RetanguloTeste {
 
     public static void main(String[] args){
-        Retangulo retangulo = new Retangulo();
 
         Double comprimento = Double.valueOf(args[0]);
         Double largura = Double.valueOf(args[1]);
 
-        double resultado = retangulo.Calcular(comprimento, largura);
+        Retangulo retangulo = new Retangulo(comprimento, largura);
 
-        System.out.println("Resultado: " + resultado);
-    }
+        System.out.println(retangulo.toString());
+}
 }
